91babb6f111ba8be369a51fc26bae518f63d8c4e
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-03-20 10:48:50 +0000

    first porting from JetAnalysis

---
+++ test/jetexample.C
@@ -0,0 +1,9 @@
+{
+   gSystem->Load("libPhysicsToolsFWLite");
+   AutoLibraryLoader::enable();
+   TChain jets("Events");
+   jets.Add("jets.root");
+   gSystem->Load( "libPhysicsToolsParallelAnalysis );
+   JetTSelector * selector = new JetTSelector;
+   jets.Process( selector );
+}
